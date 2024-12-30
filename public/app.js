a7f7a6c3222e6a19b8b15058d4dcdb76eccb186f
RevanSP <revanspstudy28@gmail.com>
2024-12-31 05:12:03 +0700

    update index.html

--- public/app.js
+++ public/app.js
@@ -11,20 +11,35 @@ document.addEventListener("DOMContentLoaded", () => {
       const reader = new FileReader();
       reader.onload = function (e) {
         base64Output.value = e.target.result;
-        downloadBase64Btn.innerHTML = 'Download as File';  
+        downloadBase64Btn.innerHTML = 'COPY';
+        toggleCopyButton();  
       };
 
       reader.readAsDataURL(file);
     }
   });
 
+  function toggleCopyButton() {
+    const base64String = base64Output.value;
+    if (base64String) {
+      downloadBase64Btn.disabled = false; 
+    } else {
+      downloadBase64Btn.disabled = true;  
+    }
+  }
+
   downloadBase64Btn.addEventListener('click', () => {
     const base64String = base64Output.value;
     if (base64String) {
-      const link = document.createElement('a');
-      link.href = base64String;
-      link.download = "file_from_base64";
-      link.click();
+      navigator.clipboard.writeText(base64String).then(() => {
+        downloadBase64Btn.innerHTML = 'COPIED';
+        
+        setTimeout(() => {
+          downloadBase64Btn.innerHTML = 'COPY';
+        }, 3000);  
+      }).catch(err => {
+        alert("Failed to copy: " + err);
+      });
     } else {
       alert("Please convert a file to Base64 first.");
     }
